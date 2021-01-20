d6c8636cc0856f1dad32f081660130b10661d693
germanny <germanny@gmail.com>
2021-01-20 17:58:23 -0600

    Update index.js

--- client/src/Components/Context/index.js
+++ client/src/Components/Context/index.js
@@ -1,32 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import Data from '../../Data';
+import React, { useState, useEffect } from "react";
+import Data from "../../Data";
 
 export const Context = React.createContext();
 
 export const Provider = (props) => {
   const [courseData, setCourseData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [courseId, setCourseId] = useState('');
+  const [courseId, setCourseId] = useState("");
 
   useEffect(() => {
     const data = new Data();
     console.log(data);
 
-    data.searchCourseApi(`/courses/${courseId}`)
-      .then(response => setCourseData(response.data))
-      .catch(error => console.log('Error fetching and parsing data', error))
+    data
+      .searchCourseApi(`/courses/${courseId}`)
+      .then((response) => setCourseData(response.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
       .finally(() => setIsLoading(false));
   }, [courseId]);
 
   return (
-    <Context.Provider value={{
-      courseData,
-      isLoading,
-      actions: {
-        setCourseId
-      }
-    }}>
-      { props.children}
+    <Context.Provider
+      value={{
+        courseData,
+        isLoading,
+        actions: {
+          setCourseId,
+        },
+      }}
+    >
+      {props.children}
     </Context.Provider>
   );
-}
+};
